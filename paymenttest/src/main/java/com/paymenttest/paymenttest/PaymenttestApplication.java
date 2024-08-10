15360f64f78124e0eed17dccaeca6a8a935e153d
thiago <thiagolaass202@gmail.com>
2024-08-10 18:18:30 -0300

    ..

---
+++ paymenttest/src/main/java/com/paymenttest/paymenttest/PaymenttestApplication.java
@@ -0,0 +1,13 @@
+package com.paymenttest.paymenttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymenttestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymenttestApplication.class, args);
+	}
+
+}
---
+++ paymenttest/src/test/java/com/paymenttest/paymenttest/PaymenttestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paymenttest.paymenttest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymenttestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
